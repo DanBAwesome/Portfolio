46c1fd2544ed7f2e1325c0176ce04ef6b50749ca
Daniel <dnlbshff@gmail.com>
2020-06-03 18:26:16 +0100

    Swapped Portfolio With About Page. Image scroll popup added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,35 @@
 var nav = document.getElementById("navBar");
+var imgPopup = document.getElementById("scrollText");
+var imgScroll = document.getElementById("sites");
 $(document).ready(
     function () {
-        nav.addEventListener("click", navClick)
+        nav.addEventListener("click", navClick);
+        if (window.innerWidth < 992) {
+            imgScroll.addEventListener("scroll", checkScrollMobile, { once: true });
+        }
     }
 )
 
 function navClick() {
     if (nav.classList.contains("show")) {
-        nav.classList.remove("show")
+        nav.classList.remove("show");
     }
+}
+
+function checkScrollMobile() {
+    var popupOpacity = imgPopup.style.opacity = 1;
+    var id = setInterval(frame, 20);
+
+    function frame() {
+        if (popupOpacity <= 0) {
+            clearInterval(id);
+            imgPopup.style.opacity = 0;
+        }
+        else {
+            popupOpacity -= 0.10
+            imgPopup.style.opacity = popupOpacity; 
+            console.log(imgPopup.style.opacity);
+        }
+    }
+
 }
